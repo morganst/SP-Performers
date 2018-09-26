03208d3aa886a8b1ed31a72a0d14675fba29ba87
Morgan Sturtz <msturtz@my.apsu.edu>
2018-09-25 20:43:20 -0400

    added route for home, which displays all students names

--- resources/js/components/Main.js
+++ resources/js/components/Main.js
@@ -4,49 +4,11 @@ import Navbar from './Navbar/Navbar';
  
 /* Main Component */
 class Main extends Component {
- 
-  constructor() {
-   
-    super();
-    //Initialize the state in the constructor
-    this.state = {
-        students: [],
-    }
-  }
-  /*componentDidMount() is a lifecycle method
-   * that gets called after the component is rendered
-   */
-  componentDidMount() {
-    /* fetch API in action */
-    fetch('/api/student')
-        .then(response => {
-            return response.json();
-        })
-        .then(students => {
-            this.setState({ students });
-        });
-  }
- 
- renderStudents() {
-    return this.state.students.map(student => {
-        return (
-            /* When using list you need to specify a key
-             * attribute that is unique for each list item
-            */
-            <li key={student.id} >
-                { student.firstName } 
-            </li>      
-        );
-    })
-  }
    
   render() {
     return (
         <div>
             <Navbar />
-            <ul>
-            { this.renderStudents() }
-            </ul> 
         </div> 
        
     );
--- resources/js/components/Navbar/Navbar.js
+++ resources/js/components/Navbar/Navbar.js
@@ -1,21 +1,37 @@
 import React from 'react';
-
+import { Link, BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from '../Home/Home';
 import navbar from './css/navbar.css';
 
 export default () => {
   return (
-    <div className="navbar">
-        <div className="container">
-            <div className="logo"><img src="/images/logo.png" width="90px"></img></div>
-            <nav>
-                <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">Classes</a></li>
-                <li><a href="#">Students</a></li>
-                </ul>
-            </nav>
+      <Router>
+        <div>
+          <div className="navbar">
+              <div className="container">
+                <div className="logo"><img src="/images/logo.png" width="90px"></img></div>
+                  <div>
+                    <nav>
+                      <OldSchoolMenuLink to="/home" label="Home" />
+                    </nav>
+                  </div>
+              </div>
+          </div>
+        <Route path="/home" component={Home} />
         </div>
-    </div>
+      </Router>
   )
 }
 
+
+const OldSchoolMenuLink = ({ to, label }) => (
+  <Route
+    path={to}
+    children={({ match }) => (
+      <div className={match ? "active" : ""}>
+        {match ? "> " : ""}
+        <Link to={to}>{label}</Link>
+      </div>
+    )}
+  />
+);
