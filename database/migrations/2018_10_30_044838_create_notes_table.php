47d4c6a9c21e214016f28671e5da492c232a444f
Thegns <thegns18@gmail.com>
2018-10-30 01:05:19 -0400

    Made a Notes controller and migration

---
+++ database/migrations/2018_10_30_044838_create_notes_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notes', function (Blueprint $table) {
+            $table->increments('NId');
+            $table->timestamps();
+            $table->string('I/B');
+            $table->string('Instructor');
+            $table->string('Class');
+            $table->integer('SID');
+            $table->mediumText('Text');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notes');
+    }
+}
