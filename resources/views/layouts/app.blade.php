3be09e895b23983cf9bdf9e0c0d4ea1cb5be21b8
Brandon Bradlee <brandon.bradlee@gmail.com>
2018-12-31 17:31:00 -0500

    Required Field alert

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,7 +23,15 @@
 <body>
     <div id="app">
         @include('inc.navbar')
+        @if ($errors->any())
+        <div class="alert alert-danger">
 
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+
+        </div>
+        @endif
         <main class="py-4">
             @yield('content')
         </main>
