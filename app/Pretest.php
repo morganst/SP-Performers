635f8c10f42dc13adfb5df90a0ca89c8a9bfa249
Morgan Sturtz <msturtz@my.apsu.edu>
2019-01-27 15:27:52 -0500

    once again fixing heroku

--- app/Pretest.php
+++ app/Pretest.php
@@ -12,7 +12,7 @@ class Pretest extends Model
         'Q7', 'Q8', 'Q9', 'Q10', 'Q11', 'Q12', 'Q13', 'Q14', 
         'Q15', 'Q16', 'Q17', 'Q18', 'Q19', 'Q20'];
 
-    protected $table = 'Pretest';
+    protected $table = 'pretest';
 
     public $primaryKey = 'id';
 
