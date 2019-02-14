6b1142c9f00194a94cca53990d5981ad9a14edf6
Thegns <thegns18@gmail.com>
2019-02-14 00:28:18 -0500

    updated filter and changed the seeder to match class location

--- app/Http/Controllers/FilterController.php
+++ app/Http/Controllers/FilterController.php
@@ -8,7 +8,7 @@ class FilterController extends Controller
 {
     public function filter(){
         $searchkey = \Request::get('title');
-        $classes =  Classes::where('status', 'like', '' .$searchkey. '')->orderBy('created_at', 'des')->paginate(10);
-        return view('volunteers/search', ['volunteers' => $volunteers]);
+        $classes =  Classes::where('location', 'like', '' .$searchkey. '')->orderBy('created_at', 'des')->paginate(10);
+        return view('Classes/search', ['classes' => $classes]);
     }
 }
