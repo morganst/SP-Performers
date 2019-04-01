7079a054350ace8330fc03765fdd57983b3d71f8
bbradlee <brandon.bradlee@gmail.com>
2019-04-01 10:18:32 -0400

    Fix footer to the bottom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,6 +20,7 @@
 
 
     <!-- Styles -->
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
     <link href="{{ asset('css/custom.css') }}" rel="stylesheet">
 </head>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,6 @@
 
 @section('content')
 
-<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-
-
 <div><h2>Dashboard</h2></div>
 
 
